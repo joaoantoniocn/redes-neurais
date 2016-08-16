9b8ca3f441d075b5111e627b8af1a956655c0b99
João Antônio Chagas Nunes <joaoantoniocn@gmail.com>
2016-08-16 20:26:23 -0300

    Update Perceptron.java

--- src/exercicio1/Perceptron.java
+++ src/exercicio1/Perceptron.java
@@ -1,5 +1,7 @@
 package exercicio1;
 
+import java.util.Random;
+
 public class Perceptron {
 	
 	private double[] w;
@@ -8,24 +10,32 @@ public class Perceptron {
 	
 	private int numEpocas;
 	private int maxEpocas;
+	private int error;
 	
 	public Perceptron(int numDimensions, double[][] matriz){
-		this.w  = new double[numDimensions];
+		this.w  = new double[numDimensions+1];
 		this.matriz = matriz;
-		limear = 0.5;
+		limear = 1;
 		numEpocas = 0;
-		maxEpocas = 50;
+		maxEpocas = 100;
 	}
 	
 	public int run(double[] e){
 		int saida = 0;
-		double soma = 0;
+		double soma = w[0];
 		
 		for(int i=0; i < w.length; i++){
-			soma += e[i] * w[i];
+			
+			if(i==0){
+				soma += 1 * w[i];
+			}else{
+				soma += e[i-1] * w[i];
+			}
+			
+			
 		}
 		
-		if(soma > limear){
+		if(soma > 0){
 			saida = 1;
 		}
 		
@@ -35,7 +45,7 @@ public class Perceptron {
 	public void treinar(){
 		
 		boolean treinado = true;
-		
+		error = 0;
 		//System.out.println("----------------------- epoca "+ numEpocas);
 		
 		// percorre todos os exemplos
@@ -59,12 +69,66 @@ public class Perceptron {
 		
 	}
 	
+	public void treinarAleatorio(){
+		
+		boolean treinado = true;
+		error = 0;
+		//System.out.println("----------------------- epoca "+ numEpocas);
+		
+		// percorre todos os exemplos
+		for(int i=0; i< matriz.length; i++){
+			Random r = new Random();
+			int indice = r.nextInt(matriz.length);
+			int saida = run(matriz[indice]);
+			
+			// checa a saida como valor esperado
+			if(saida != matriz[indice][matriz[1].length-1]){
+				updateW(matriz[indice], saida, (int) matriz[indice][matriz[1].length-1]);
+				treinado = false;
+			}
+			
+		}
+		
+		numEpocas++;
+		
+		if((!treinado) && (numEpocas<maxEpocas)){
+			treinar();
+		}
+		
+	}
+	
+	public int calcularError(){
+		
+		error = 0;
+		//System.out.println("----------------------- epoca "+ numEpocas);
+		
+		// percorre todos os exemplos
+		for(int i=0; i< matriz.length; i++){
+			
+			int saida = run(matriz[i]);
+			
+			// checa a saida como valor esperado
+			if(saida != matriz[i][matriz[1].length-1]){
+				error++;			
+			}
+			
+		}
+		
+		return error;
+	}
+	
 	private void updateW(double[] e, int saida, int esperado){
 		
 		for(int i=0; i < w.length; i++){
-			w[i] += (esperado - saida) * e[i];
+			
+			if(i==0){
+				w[i] += (esperado - saida) * 1;
+			}else{
+				w[i] += (esperado - saida) * e[i-1];
+			}
+			
 		}
-		
+		error++;
 		//System.out.println("w0 = "+w[0]+" | w1 = "+w[1]+" | w2 = "+w[2]+" | w3 = "+w[3]);
 		
 	}
@@ -100,5 +164,22 @@ public class Perceptron {
 		this.limear = limear;
 	}
 
+	public int getError() {
+		return error;
+	}
+
+	public void setError(int error) {
+		this.error = error;
+	}
+
+	public int getNumEpocas() {
+		return numEpocas;
+	}
+
+	public void setNumEpocas(int numEpocas) {
+		this.numEpocas = numEpocas;
+	}
+
+	
 	
 }
