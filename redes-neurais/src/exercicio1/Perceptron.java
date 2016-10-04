148785b6fc4dc93d9b3f0f86ee5e8bcc65735349
Joao Antonio C. Nunes <joaoantoniocn@gmail.com>
2016-10-04 09:29:04 -0300

    Relatorio perceptron

--- redes-neurais/src/exercicio1/Perceptron.java
+++ redes-neurais/src/exercicio1/Perceptron.java
@@ -1,20 +1,59 @@
 package exercicio1;
 
+import java.util.Random;
+
 public class Perceptron {
 	
 	private double[] w;
 	private double[][] matriz; // ultima posicao guarda o valor esperado
-	private double limear;
+	private double taxaAprendizagem;
 	
 	private int numEpocas;
 	private int maxEpocas;
+	private int error;
 	
-	public Perceptron(int numDimensions, double[][] matriz){
-		this.w  = new double[numDimensions];
+	// --- MLP ---
+	private double lambda;
+	private double errorMLP; // erro usado para atualizar os pesos da MLP
+	
+	public Perceptron(double[][] matriz){
+		
+		this.w  = new double[matriz[1].length];
 		this.matriz = matriz;
-		limear = 0.5;
+		taxaAprendizagem = 1;
 		numEpocas = 0;
-		maxEpocas = 50;
+		maxEpocas = 100;
+	}
+	
+	// MLP
+	public Perceptron(){
+		lambda = 0;
+	}
+	
+	public double runMLP(double[] e){
+		double saida = 0;
+		double soma = 0;
+		
+		for(int i=0; i < w.length; i++){
+			
+			if(i==0){
+				soma += 1 * w[i];
+			}else{
+				soma += e[i-1] * w[i];
+			}
+			
+			
+		}
+		
+		saida = sigmoide(soma);
+		
+		return saida;
+	}
+	
+	private double sigmoide(double output){
+		double result = 1/(1 + Math.pow(Math.E, lambda*output*-1));
+		
+		return result;
 	}
 	
 	public int run(double[] e){
@@ -22,10 +61,17 @@ public class Perceptron {
 		double soma = 0;
 		
 		for(int i=0; i < w.length; i++){
-			soma += e[i] * w[i];
+			
+			if(i==0){
+				soma += 1 * w[i];
+			}else{
+				soma += e[i-1] * w[i];
+			}
+			
+			
 		}
 		
-		if(soma > limear){
+		if(soma > 0){
 			saida = 1;
 		}
 		
@@ -35,7 +81,7 @@ public class Perceptron {
 	public void treinar(){
 		
 		boolean treinado = true;
-		
+		error = 0;
 		//System.out.println("----------------------- epoca "+ numEpocas);
 		
 		// percorre todos os exemplos
@@ -59,14 +105,77 @@ public class Perceptron {
 		
 	}
 	
+	public void treinarAleatorio(){
+		double[] temp;
+		
+		for(int i=0; i< matriz.length; i++){
+			
+			if((i%3) == 0){
+				temp = matriz[i];
+				
+				matriz[i] = matriz[matriz.length-i-1];
+				matriz[matriz.length-1-i] = temp;
+			}
+			
+		}
+		
+		treinar();
+	}
+	
+	public int calcularError(){
+		
+		error = 0;
+		//System.out.println("----------------------- epoca "+ numEpocas);
+		
+		// percorre todos os exemplos
+		for(int i=0; i< matriz.length; i++){
+			
+			int saida = run(matriz[i]);
+			
+			// checa a saida como valor esperado
+			if(saida != matriz[i][matriz[1].length-1]){
+				error++;			
+			}
+			
+		}
+		
+		return error;
+	}
+	
 	private void updateW(double[] e, int saida, int esperado){
 		
 		for(int i=0; i < w.length; i++){
-			w[i] += (esperado - saida) * e[i];
+			
+			if(i==0){
+				w[i] += taxaAprendizagem * (esperado - saida) * 1;
+			}else{
+				w[i] += taxaAprendizagem * (esperado - saida) * e[i-1];
+			}
+			
 		}
-		
+		error++;
 		//System.out.println("w0 = "+w[0]+" | w1 = "+w[1]+" | w2 = "+w[2]+" | w3 = "+w[3]);
 		
+	}
+	
+	public void printBase(){
+		for (int i = 0; i < matriz.length; i++) {
+			System.out.println();
+			for (int j = 0; j < w.length; j++) {
+				//System.out.print(w.length);
+				
+				if(j==0){
+					System.out.print(i + ": ");
+				}
+				System.out.print(matriz[i][j]);
+				if(j<(w.length-2)){
+					System.out.print(",");
+				}else if(j == (w.length-2)){
+					System.out.print(" - ");
+				} 
+			}
+
+		}
 	}
 	
 	public void printW(){
@@ -74,6 +183,11 @@ public class Perceptron {
 			System.out.print(w[i]);
 			System.out.print(" | ");
 		}
+	}
+	
+	// seta o numero de pesos
+	public void setWMLP(int tamanho){
+		this.w = new double[tamanho];
 	}
 	
 	public double[] getW() {
@@ -92,13 +206,47 @@ public class Perceptron {
 		this.matriz = matriz;
 	}
 
-	public double getLimear() {
-		return limear;
+	public double getTaxaAprendizagem() {
+		return taxaAprendizagem;
 	}
 
-	public void setLimear(double limear) {
-		this.limear = limear;
+	public void setTaxaAprendizagem(double taxaAprendizagem) {
+		this.taxaAprendizagem = taxaAprendizagem;
 	}
 
+	public int getError() {
+		return error;
+	}
+
+	public void setError(int error) {
+		this.error = error;
+	}
+
+	public int getNumEpocas() {
+		return numEpocas;
+	}
+
+	public void setNumEpocas(int numEpocas) {
+		this.numEpocas = numEpocas;
+	}
+
+	public double getLambda() {
+		return lambda;
+	}
+
+	public void setLambda(double lambda) {
+		this.lambda = lambda;
+	}
+
+	public double getErrorMLP() {
+		return errorMLP;
+	}
+
+	public void setErrorMLP(double errorMLP) {
+		this.errorMLP = errorMLP;
+	}
+
+	
+	
 	
 }
